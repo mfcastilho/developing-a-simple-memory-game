f513fc02ee03a9d5cfed9317ff3a45cfeb1084c1
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2022-12-10 03:33:06 -0300

    created the click event and the functions of the system

--- src/public/js/flipTheCard.js
+++ src/public/js/flipTheCard.js
@@ -1,22 +1,65 @@
 
 const cards = document.querySelectorAll(".card-box");
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
 
-let count = 0;
-
-let index = "";
 
 
 function flipTheCard(event){
+  if(lockBoard){
+    return;
+  }
 
-  
-  this.classList.toggle("flip");
-  
+
+  this.classList.add("flip"); 
+
+  if(!hasFlippedCard){
+    hasFlippedCard =true;
+    firstCard = this;
+    console.log(firstCard);
+    return;
+  }
+
+  secondCard = this;
+  hasFlippedCard = false;
+  console.log(secondCard);
+  checkForMatch();
 }
 
 
+function checkForMatch(){
+  const namefirstCard = firstCard.getAttribute("name");
+  const nameSecondCard = secondCard.getAttribute("name")
+
+  if(namefirstCard === nameSecondCard){
+    disableCards();
+    return;
+  }
+
+  unflipCards();
+}
+
+function disableCards(){
+  firstCard.removeEventListener("click", flipTheCard);
+  secondCard.removeEventListener("click", flipTheCard);
+  // firstCard.style.opacity = "0.8";
+  // secondCard.style.opacity = "0.8";
+}
+
+function unflipCards(){
+  lockBoard = true;
+  setTimeout(()=>{
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    lockBoard = false;
+  }, 1500);
+}
+
+
+//Evento de click
 cards.forEach(card=>{
-  
- 
-  card.addEventListener("click",flipTheCard);
- 
+  card.addEventListener("click",flipTheCard); 
 });
+
+
