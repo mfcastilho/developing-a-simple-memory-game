86eb4563e2c7b5a6458110dbca2b3f6026a57c84
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2022-12-11 21:05:27 -0300

    Update some images and  fix the homeController.js

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -3,13 +3,9 @@ const CardController = require("./CardController.js")
 
 const HomeController = {
   showHome: (req, res)=>{
-    const firstArray = CardController.shufflingCards();
-    const secondArray = CardController.shufflingCards();
-
-    const newArray = firstArray.concat(secondArray);
-
-
-    return res.render("index.ejs", {dataBase:newArray});
+    const array = CardController.shufflingCards();
+    
+    return res.render("index.ejs", {dataBase:array});
   }
   
 }
--- src/public/js/flipTheCard.js
+++ src/public/js/flipTheCard.js
@@ -4,29 +4,40 @@ let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
 
+let muteButton = document.querySelector(".mute-button");
+
+let startAudio = true;
+
+let countTotalMatchs = 0;
+
 
 
 function flipTheCard(event){
-  if(lockBoard){
-    return;
-  }
+  if(lockBoard) return;
+  
+  //Tratamento para o duplo clique
+  if(this === firstCard) return;
 
 
   this.classList.add("flip"); 
 
   if(!hasFlippedCard){
-    hasFlippedCard =true;
+    hasFlippedCard = true;
     firstCard = this;
     console.log(firstCard);
+
+    const dinoName = firstCard.getAttribute("name")
+    audioDinoName(dinoName);
     return;
   }
 
   secondCard = this;
   hasFlippedCard = false;
   console.log(secondCard);
+  audioDinoName(secondCard.getAttribute("name"));
+  console.log("\n---------------//----------");
   checkForMatch();
 }
-
 
 function checkForMatch(){
   const namefirstCard = firstCard.getAttribute("name");
@@ -34,6 +45,12 @@ function checkForMatch(){
 
   if(namefirstCard === nameSecondCard){
     disableCards();
+    countTotalMatchs++;
+    if(countTotalMatchs < 16){
+      audioFoundTheMatch(namefirstCard);
+    }else if(countTotalMatchs == 16){
+      audioFinishingTheGame();
+    }
     return;
   }
 
@@ -43,8 +60,11 @@ function checkForMatch(){
 function disableCards(){
   firstCard.removeEventListener("click", flipTheCard);
   secondCard.removeEventListener("click", flipTheCard);
-  // firstCard.style.opacity = "0.8";
+  // firstCard.style.opacity = "0.5";
   // secondCard.style.opacity = "0.8";
+
+
+  resetBoard();
 }
 
 function unflipCards(){
@@ -52,14 +72,92 @@ function unflipCards(){
   setTimeout(()=>{
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
-    lockBoard = false;
+    
+
+    resetBoard();
   }, 1500);
 }
 
 
+function resetBoard(){
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+function audioDinoName(dinoName){
+
+  if(!startAudio){
+    return;
+  }
+
+  console.log(dinoName);
+  let audio = new Audio();
+  const url = `../sounds/${dinoName}.mp3`;
+  console.log(url);
+  audio.src = url;
+
+  setTimeout(()=>{
+    audio.play();
+  }, 500)
+  
+}
+
+function audioFoundTheMatch(dinoName){
+
+  if(!startAudio){
+    return;
+  }
+
+  
+  let audio1 = new Audio();
+  const url1 = `../sounds/Parabéns-Encontrou-a-dupla-de.mp3`;
+  audio1.src = url1;
+
+  let audio = new Audio();
+  const url = `../sounds/${dinoName}s.mp3`;
+  audio.src = url;
+
+  lockBoard = true;
+  setTimeout(()=>{
+    audio1.play();
+    setTimeout(()=>{
+      audio.play();
+    },3000);
+    
+    setTimeout(()=>{
+      lockBoard = false;
+    },4000)
+  }, 2500)
+  
+}
+
+function audioFinishingTheGame(){
+
+  if(!startAudio){
+    return;
+  }
+  let audio = new Audio();
+  const url = `../sounds/parabens-voce-terminou-o-jogo.mp3`;
+  audio.src = url;
+
+  setTimeout(()=>{
+    audio.play();
+  }, 2500)
+}
+
 //Evento de click
 cards.forEach(card=>{
   card.addEventListener("click",flipTheCard); 
+});
+
+muteButton.addEventListener("click", ()=>{
+
+  if(startAudio){
+    startAudio = false;
+  }else{
+    startAudio = true;
+  }
+  
 });
 
 
