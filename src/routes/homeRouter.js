96b4ce03b9e78ba11d2e3b08077fe06b7e1a73a7
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2022-12-11 21:00:30 -0300

    Insert at the homeRouter the optional route '/home'

--- src/routes/homeRouter.js
+++ src/routes/homeRouter.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const HomeController = require("../controllers/HomeController.js");
 
 router.get("/", HomeController.showHome);
+router.get("/home", HomeController.showHome);
 // router.get("/", HomeController.shufflingCards);
 
 
